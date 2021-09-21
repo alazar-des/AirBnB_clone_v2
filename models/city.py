3108121741e17ada9e669f09d8ee5cb78b467196
alazar-des <alazar.des@gmail.com>
2021-09-21 11:23:33 +0300

    create table and rows for city

--- models/city.py
+++ models/city.py
@@ -2,6 +2,7 @@
 """ City Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, ForeignKey
+from sqlalchemy.orm import relationship
 from models import storage_t
 
 
@@ -12,6 +13,7 @@ class City(BaseModel, Base):
 
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
+        places = relationship('Place', backref='cities')
     else:
         state_id = ""
         name = ""
